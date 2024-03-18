5ee280f8b6e232f89665912489933ad4398ffcdd
ChernenkiyNikitych4732 <chernenkiy.nikitychsssr@gmail.com>
2024-03-18 17:53:50 +0100

    Skypro Дипломная работа

---
+++ src/main/java/ru/skypro/diplomawork/DiplomaworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.diplomawork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomaworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomaworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/diplomawork/DiplomaworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.diplomawork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiplomaworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
